e85bf24f8a291c25902d2d584b682442ddfb4fca
blakeManternach <blake.manternach411@gmail.com>
2025-03-29 12:13:22 -0500

    externalize config

---
+++ game/config.lua
@@ -0,0 +1,13 @@
+io.stdout:setvbuf("no")
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+local love_errorhandler = love.errorhandler
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
--- game/main.lua
+++ game/main.lua
@@ -1,17 +1,5 @@
-io.stdout:setvbuf("no")
-if arg[2] == "debug" then
-    require("lldebugger").start()
-end
+require("config")
 
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
-end
-
-local love_errorhandler = love.errorhandler
-function love.errorhandler(msg)
-    if lldebugger then
-        error(msg, 2)
-    else
-        return love_errorhandler(msg)
-    end
 end
